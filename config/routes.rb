5441848dcd74c531b3b209206f54cfe905261dd1
danielleoverman <danielleoverman1@gmail.com>
2017-05-18 13:33:51 -0500

    Assignment 24 Sign Up Completed

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  post 'users/confirm' => 'users#confirm'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
