d2c3b5c854678022f2708d1130173b91689930c5
danielleoverman <danielleoverman1@gmail.com>
2017-05-24 21:41:59 -0400

    Checkpoint 31 Favorites completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,37 +1,37 @@
  require 'rails_helper'
 
  RSpec.describe Post, type: :model do
-
+   let(:name) { RandomData.random_sentence }
+   let(:description) { RandomData.random_paragraph }
+   let(:title) { RandomData.random_sentence }
+   let(:body) { RandomData.random_paragraph }
+ 
    let(:topic) { Topic.create!(name: name, description: description) }
-
+ 
    let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-
    let(:post) { topic.posts.create!(title: title, body: body, user: user) }
 
    it { is_expected.to have_many(:comments) }
 
    it { is_expected.to have_many(:votes) }
+
+   it { is_expected.to have_many(:favorites) }
  
    it { is_expected.to belong_to(:topic) }
 
    it { is_expected.to belong_to(:user) }
 
    it { is_expected.to validate_presence_of(:user) }
-
-   it "has a title, body, and user attribute" do
-      expect(post).to have_attributes(title: title, body: body, user: user)
-
    it { is_expected.to validate_presence_of(:title) }
    it { is_expected.to validate_presence_of(:body) }
    it { is_expected.to validate_presence_of(:topic) }
  
    it { is_expected.to validate_length_of(:title).is_at_least(5) }
    it { is_expected.to validate_length_of(:body).is_at_least(20) }
- 
 
    describe "attributes" do
-     it "has a title and body attribute" do
-       expect(post).to have_attributes(title: title, body: body)
+     it "has a title, body and user attribute" do
+       expect(post).to have_attributes(title: title, body: body, user: user)
      end
    end
 
@@ -62,7 +62,6 @@
      end
 
      describe "#update_rank" do
-
        it "calculates the correct rank" do
          post.update_rank
          expect(post.rank).to eq (post.points + (post.created_at - Time.new(1970,1,1)) / 1.day.seconds)
@@ -80,6 +79,5 @@
          expect(post.rank).to eq (old_rank - 1)
        end
      end
-   end
-  end 
-end
+  end
+end 
